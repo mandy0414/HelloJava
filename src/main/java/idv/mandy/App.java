6275f474075d3a83ee38965ae0e1b32c97da9918
mandy-PC\mandy <gb19860414@gmail.com>
2017-07-27 20:10:53 +0800

    helloworld

--- src/main/java/idv/mandy/App.java
+++ src/main/java/idv/mandy/App.java
@@ -2,12 +2,16 @@ package idv.mandy;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            System.out.println("Hello World!");
+        }
+        int a = 1;
+        System.out.println("a*2=" + (a * 2));
+        int b = 1;
+        System.out.println("b*2=" + b *2);
     }
 }
+
