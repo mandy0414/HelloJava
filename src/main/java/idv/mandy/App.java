8e4599c1c80d5777fdc64c591162ca7303f915f2
mandy-PC\mandy <gb19860414@gmail.com>
2017-08-31 19:42:46 +0800

    九九乘法表

--- src/main/java/idv/mandy/App.java
+++ src/main/java/idv/mandy/App.java
@@ -11,13 +11,21 @@ public class App {
         int a = 1;
         System.out.println("a*2=" + (a * 2));
         int b = 1;
-        System.out.println("b*2=" + b *2);
+        System.out.println("b*2=" + b * 2);
 
         int c = 0;
-        for (int i = 1; i <= 100; i++){
+        for (int i = 1; i <= 100; i++) {
             c += i;
         }
         System.out.println(c);
+
+        Calculator myCalculator = new Calculator();
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + "　*　" + j + "　=　" + myCalculator.multiply(i, j));
+            }
+        }
     }
+
 }
 
